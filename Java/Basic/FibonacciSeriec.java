b939d133e6c62b64ac607f425f850f1ad5868bf2
Samarth Nandekar <hereaditi1@gmail.com>
2024-07-25 23:17:44 -0400

    Fibonacci Series Ques

---
+++ Java/Basic/FibonacciSeriec.java
@@ -0,0 +1,20 @@
+class FibonacciSeries{
+
+    static void Fibonacci(int N){
+        int num1 = 0;
+        int num2 = 1;
+        for(int i = 0; i < N ; i++){
+            System.out.println(num1 + " ");
+
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+        }
+    }
+
+    public static void main(String[] args){
+        int N = 10;
+        Fibonacci(N);
+
+    }
+}
